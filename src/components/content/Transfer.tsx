4ce5a07d2158e3d483535fcceb38ce953d7a2986
Constantin Danilov <Constantin.Danilov@endava.com>
2023-05-31 15:37:27 +0300

    finished transfer block and made a slider

--- src/components/content/Transfer.tsx
+++ src/components/content/Transfer.tsx
@@ -1,18 +1,51 @@
-import React from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { TransferOption } from './TransferOption/TransferOption';
 import { ReactComponent as VisaCardSVG } from 'assets/card-visa.svg';
 import { ReactComponent as MasterCardSVG } from 'assets/card-master.svg';
 import { ReactComponent as TransferPercentageSVG } from 'assets/transfer-percentage.svg';
 import { ReactComponent as TransferArrowRightSVG } from 'assets/arrow-right-tail.svg';
+import { ReactComponent as ArrowLeftSVG } from 'assets/arrow-left.svg';
+import { ReactComponent as ArrowRightSVG } from 'assets/arrow-right.svg';
 import { Input } from 'components/UI/Input/Input';
 import { Button } from 'components/UI/Button/Button';
 
 const transferOptions = [
   { svg: <VisaCardSVG />, type: '3149 Debit card', currency: '2895.15 USD' },
-  { svg: <MasterCardSVG />, type: 'Debit card...', currency: 'EUROPIAN...' },
+  { svg: <MasterCardSVG />, type: '1245 Debit card', currency: '2345.15 USD' },
+  { svg: <MasterCardSVG />, type: '1122 Debit card', currency: '1235.15 USD' },
+  { svg: <VisaCardSVG />, type: '1322 Debit card', currency: '835.15 USD' },
 ];
 
 export const Transfer = () => {
+  const optionsRef = useRef<HTMLDivElement>(null);
+  const [canScrollLeft, setCanScrollLeft] = useState(false);
+  const [canScrollRight, setCanScrollRight] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollLeft = optionsRef.current?.scrollLeft;
+      const scrollWidth = optionsRef.current?.scrollWidth;
+      const clientWidth = optionsRef.current?.clientWidth;
+
+      if (scrollLeft && clientWidth && scrollWidth) {
+        setCanScrollLeft(scrollLeft > 10);
+        setCanScrollRight(scrollLeft + clientWidth < scrollWidth);
+      }
+    };
+    optionsRef.current?.addEventListener('scroll', handleScroll);
+
+    return () => {
+      optionsRef.current?.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  const onOptionsScroll = (direction: string) => {
+    optionsRef.current?.scrollBy({
+      left: direction === 'left' ? -200 : 200,
+      behavior: 'smooth',
+    });
+  };
+
   return (
     <div className="transfer">
       <div className="transfer__top">
@@ -24,9 +57,27 @@ export const Transfer = () => {
       </div>
 
       <div className="transfer__options">
-        {transferOptions.map((option, ind) => {
-          return <TransferOption key={ind} {...option} />;
-        })}
+        <div className="transfer__options__wrapper" ref={optionsRef}>
+          {transferOptions.map((option, ind) => {
+            return <TransferOption key={ind} {...option} />;
+          })}
+        </div>
+        <button
+          className={`transfer__scroll-left ${
+            canScrollLeft ? 'visible' : 'hidden'
+          }`}
+          onClick={() => onOptionsScroll('left')}
+        >
+          <ArrowLeftSVG/>
+        </button>
+        <button
+          className={`transfer__scroll-right ${
+            canScrollRight ? 'visible' : 'hidden'
+          }`}
+          onClick={() => onOptionsScroll('right')}
+          >
+          <ArrowRightSVG/>
+        </button>
       </div>
 
       <div className="transfer__inputs">
